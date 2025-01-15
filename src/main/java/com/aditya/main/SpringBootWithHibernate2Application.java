8a2c06b10f7df6a44d6e56ca977e615b4e9a0bfa
Aditya-Prakash07 <prakashaditya775@gmail.com>
2025-01-15 22:41:22 +0530

    Working perfectly but it is not a good practice because we should not make object of SessionFactory again and agian because it is heavy.. The BEST PRACTICE WE WILL SEE IN SpringBootWithHibernate3-2025

--- src/main/java/com/aditya/main/SpringBootWithHibernate2Application.java
+++ src/main/java/com/aditya/main/SpringBootWithHibernate2Application.java
@@ -1,13 +1,39 @@
 package com.aditya.main;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.aditya.main.entities.User;
 
 @SpringBootApplication
 public class SpringBootWithHibernate2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootWithHibernate2Application.class, args);
+
+		Configuration cfg = new Configuration();
+		cfg.configure("com/aditya/main/config/hibernate.cfg.xml");
+		SessionFactory sessionFactory = cfg.buildSessionFactory();
+		Session session = sessionFactory.openSession();
+		Transaction transaction = session.beginTransaction();
+
+		try {
+			User user = session.get(User.class, 33L);
+			if (user != null) {
+				System.out.println(user.getName());
+				System.out.println(user.getEmail());
+			} else {
+				System.out.println("Cannot find User Details..#..#");
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
 
 }
